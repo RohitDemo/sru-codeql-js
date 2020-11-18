25559fb94e8142bc5c8bba3b6ca4f146730ffd40
rohitnb <rohitnb@github.com>
2020-11-18 21:41:18 +0530

    rate limited

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+app.use(limiter);
+
+
 const path = __dirname + '/views/';
 const port = 8080;
 
@@ -11,10 +20,14 @@ router.use(function (req,res,next) {
 });
 
 router.get('/', function(req,res){
+  let path = req.params.path;
+  if (isValidPath(path))
   res.sendFile(path + 'index.html');
 });
 
 router.get('/sharks', function(req,res){
+  let path = req.params.path;
+  if (isValidPath(path))
   res.sendFile(path + 'sharks.html');
 });
 
